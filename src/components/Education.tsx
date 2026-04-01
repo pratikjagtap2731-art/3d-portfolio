f0348d4b6baebb144065b8fb413af5c5526fdd14
Pratik Jagtap <pratikjagtap@Pratiks-MacBook-Air.local>
2026-04-01 15:43:13 -0700

    feat: Fix Work logo spacing and add mobile expandable courses to Education

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import "./styles/Education.css";
+import { MdExpandMore } from "react-icons/md";
 
 const Education = () => {
+  const [expandedDuke, setExpandedDuke] = useState(false);
+  const [expandedViit, setExpandedViit] = useState(false);
+
   return (
     <div className="education-section section-container" id="education">
       <div className="education-container">
@@ -23,12 +28,24 @@ const Education = () => {
           <h4 className="edu-degree">
             Master of Engineering Management
           </h4>
-          <div className="edu-highlights">
-            <span className="edu-tag">Product Management</span>
-            <span className="edu-tag">Design Customer Experiences</span>
-            <span className="edu-tag">Marketing</span>
-            <span className="edu-tag">Competitive Strategy</span>
-            <span className="edu-tag">IP, Business Law & Entrepreneurship</span>
+          <button 
+            className="edu-expand-btn" 
+            onClick={() => setExpandedDuke(!expandedDuke)}
+            aria-label={expandedDuke ? "Hide courses" : "Show courses"}
+            data-cursor="disable"
+          >
+            <span>{expandedDuke ? "Hide Courses" : "Show Courses"}</span>
+            <MdExpandMore className={`edu-expand-icon ${expandedDuke ? "expanded" : ""}`} />
+          </button>
+          
+          <div className={`edu-expanded-content ${expandedDuke ? "show" : ""}`}>
+            <div className="edu-highlights">
+              <span className="edu-tag">Product Management</span>
+              <span className="edu-tag">Design Customer Experiences</span>
+              <span className="edu-tag">Marketing</span>
+              <span className="edu-tag">Competitive Strategy</span>
+              <span className="edu-tag">IP, Business Law & Entrepreneurship</span>
+            </div>
           </div>
           <p className="edu-note">
             Fuqua School of Business & Pratt School of Engineering
@@ -52,12 +69,24 @@ const Education = () => {
           <h4 className="edu-degree">
             Bachelor of Information Technology
           </h4>
-          <div className="edu-highlights">
-            <span className="edu-tag">Software Development</span>
-            <span className="edu-tag">Artificial Intelligence</span>
-            <span className="edu-tag">Business Intelligence</span>
-            <span className="edu-tag">Data Science</span>
-            <span className="edu-tag">Human Computer Interaction</span>
+          <button 
+            className="edu-expand-btn" 
+            onClick={() => setExpandedViit(!expandedViit)}
+            aria-label={expandedViit ? "Hide courses" : "Show courses"}
+            data-cursor="disable"
+          >
+            <span>{expandedViit ? "Hide Courses" : "Show Courses"}</span>
+            <MdExpandMore className={`edu-expand-icon ${expandedViit ? "expanded" : ""}`} />
+          </button>
+
+          <div className={`edu-expanded-content ${expandedViit ? "show" : ""}`}>
+            <div className="edu-highlights">
+              <span className="edu-tag">Software Development</span>
+              <span className="edu-tag">Artificial Intelligence</span>
+              <span className="edu-tag">Business Intelligence</span>
+              <span className="edu-tag">Data Science</span>
+              <span className="edu-tag">Human Computer Interaction</span>
+            </div>
           </div>
         </div>
       </div>
